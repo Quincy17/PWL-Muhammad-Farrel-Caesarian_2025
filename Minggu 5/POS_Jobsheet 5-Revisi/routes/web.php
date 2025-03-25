a86f21471dfe68d3d786dee86a3d7c7c16562b7b
FarrelCaesarian <farrelcaesarian15@gmail.com>
2025-03-25 10:11:43 +0700

    Praktikum 2

--- Minggu 5/POS_Jobsheet 5-Revisi/routes/web.php
+++ Minggu 5/POS_Jobsheet 5-Revisi/routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
-
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +26,7 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/', [WelcomeController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
