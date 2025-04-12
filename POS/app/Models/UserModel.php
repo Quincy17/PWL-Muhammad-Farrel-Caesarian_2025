a01f9da8105be5cd0b221838fbfca24bacf4fdf3
FarrelCaesarian <farrelcaesarian15@gmail.com>
2025-04-12 15:12:26 +0700

    Implementasi JS3 Project UTS

--- POS/app/Models/UserModel.php
+++ POS/app/Models/UserModel.php
@@ -4,9 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Models\Level;
-use App\Models\LevelModel;
 
 class UserModel extends Model
 {
@@ -14,11 +11,4 @@ class UserModel extends Model
 
     protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
-
-    protected $fillable = ['level_id', 'username','password', 'nama'];
-
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
 }
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
-use Illuminate\Support\Facades\View;
 
 
 /*
@@ -28,9 +27,4 @@ Route::get('/', function () {
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 
