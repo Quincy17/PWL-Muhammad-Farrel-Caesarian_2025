f355f3d8ea05f119771c03cba71304100a5fc113
FarrelCaesarian <farrelcaesarian15@gmail.com>
2025-04-19 17:04:40 +0700

    Edit Dashboard

--- POS_UTS/app/Http/Controllers/WelcomeController.php
+++ POS_UTS/app/Http/Controllers/WelcomeController.php
@@ -2,17 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BarangModel;
+use App\Models\PenjualanModel;
+use App\Models\PenjualanDetailModel;
+use App\Models\StokModel;
+use App\Models\SupplierModel;
+use App\Models\UserModel;
+use App\Models\LevelModel;
+use App\Models\KategoriModel;
+use Illuminate\Support\Facades\Auth;
 class WelcomeController extends Controller
 {
     public function index()
     {
         $breadcrumb = (object) [
-            'title' => 'Selamat Datang',
+            'title' => 'Selamat Datang ' . Auth::user()->username . '!',
             'list' => ['Home', 'Welcome']
         ];
 
         $activeMenu = 'dashboard';
 
-        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+        $totalBarang = BarangModel::count();
+        $totalPenjualan = PenjualanModel::count();
+        $totalPenjualanDetail = PenjualanDetailModel::count();
+        $totalStok = StokModel::count();
+        $totalKategori = KategoriModel::count();
+        $totalSupplier = SupplierModel::count();
+        $totalUser = UserModel::count();
+        $totalLevel = LevelModel::count();
+
+        return view(
+            'welcome', 
+            ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu],
+            compact(
+                'totalBarang', 
+                'totalPenjualan', 
+                'totalPenjualanDetail', 
+                'totalStok', 
+                'totalKategori', 
+                'totalSupplier', 
+                'totalUser', 
+                'totalLevel'
+                )
+        );
     }
 }
--- POS_UTS/resources/views/welcome.blade.php
+++ POS_UTS/resources/views/welcome.blade.php
@@ -3,12 +3,98 @@
 @section('content')
 
 <div class="card">
-    <div class="card-header">
+    {{-- <div class="card-header">
         <h3 class="card-title">Hai, <span style="color:black; font-weight:bold; ">{{ Auth::user()->username }}!</span></h3>
         <div class="card-tools"></div>
-    </div>
-    <div class="card-body">
+    </div> --}}
+    {{-- <div class="card-body">
         Selamat datang {{ Auth::user()->username }}!, ini adalah halaman utama dari aplikasi ini.
+    </div> --}}
+</div>
+
+<div class="row">
+    <!-- Preview Barang -->
+    <div class="col-md-4">
+        <div class="card text-dark mb-3">
+            <div class="card-header"><h5>Jumlah Barang : {{ $totalBarang }}</h5></div>
+            <div class="card-body">
+                <a href="{{ url('/barang') }}" class="btn btn-outline-primary">Lihat Detail</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Preview Penjualan -->
+    <div class="col-md-4">
+        <div class="card text-dark mb-3">
+            <div class="card-header"><h5>Jumlah User : {{ $totalUser }}</h5></div>
+            <div class="card-body">
+                <a href="{{ url('/user') }}" class="btn btn-outline-primary">Lihat Detail</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Preview Penjualan Detail -->
+    <div class="col-md-4">
+        <div class="card text-dark mb-3">
+            <div class="card-header"><h5>Jumlah Level : {{ $totalLevel }}</h5></div>
+            <div class="card-body">
+                <a href="{{ url('/level') }}" class="btn btn-outline-primary">Lihat Detail</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <!-- Preview Stok -->
+    <div class="col-md-4">
+        <div class="card text-dark mb-3">
+            <div class="card-header"><h5>Jumlah Stok : {{ $totalStok }}</h5></div>
+            <div class="card-body">
+                <a href="{{ url('/stok') }}" class="btn btn-outline-primary">Lihat Detail</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Preview Kategori -->
+    <div class="col-md-4">
+        <div class="card text-dark mb-3">
+            <div class="card-header"><h5>Jumlah Kategori : {{ $totalKategori }}</h5></div>
+            <div class="card-body">
+                <a href="{{ url('/kategori') }}" class="btn btn-outline-primary">Lihat Detail</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Preview Supplier -->
+    <div class="col-md-4">
+        <div class="card text-dark mb-3">
+            <div class="card-header"><h5>Jumlah Supplier : {{ $totalSupplier }}</h5></div>
+            <div class="card-body">
+                <a href="{{ url('/supplier') }}" class="btn btn-outline-primary">Lihat Detail</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row justify-content-center">
+    <!-- Preview Penjualan -->
+    <div class="col-md-4">
+        <div class="card text-dark mb-3">
+            <div class="card-header"><h5>Jumlah Penjualan : {{ $totalPenjualan }}</h5></div>
+            <div class="card-body">
+                <a href="{{ url('/penjualan') }}" class="btn btn-outline-primary">Lihat Detail</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Preview Penjualan Detail -->
+    <div class="col-md-4">
+        <div class="card text-dark mb-3">
+            <div class="card-header"><h5>Detail Penjualan : {{ $totalPenjualanDetail }}</h5></div>
+            <div class="card-body">
+                <a href="{{ url('/penjualan_detail') }}" class="btn btn-outline-primary">Lihat Detail</a>
+            </div>
+        </div>
     </div>
 </div>
 
